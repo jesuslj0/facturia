249f8f6dd5f066d3e7593c6e3a15114c76428e4d
Jesús <jesuslopj0@gmail.com>
2026-02-11 02:38:41 +0100

    Arreglando Media

--- billing_ai/urls.py
+++ billing_ai/urls.py
@@ -20,3 +20,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += debug_toolbar_urls()
+
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
