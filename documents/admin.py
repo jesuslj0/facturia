bf02f0f8aa614c764c6c52368ed042d1f94b40dd
Jesús <jesuslopj0@gmail.com>
2026-02-12 03:04:48 +0100

    Arreglando admin de document

--- documents/admin.py
+++ documents/admin.py
@@ -5,8 +5,12 @@ from documents.models import Document, Company
 # Register your models here.
 @register(Document)
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ['file', 'original_name', 'document_type', 'status', 'provider_name', 'total_amount', 'created_at']
-    search_fields = ['original_name', 'status', 'provider_name', 'created_at', 'reviewed_at', 'total_amount']
+    list_display = ['file', 'original_name', 'document_type', 'status', 'total_amount', 'company_name', 'created_at']
+    search_fields = ['original_name', 'status', 'document_type', 'created_at', 'reviewed_at', 'total_amount']
+    
+    def company_name(self, obj):
+        return obj.company.name if obj.company else '-'
+
 
 @register(Company)
 class CompanyAdmin(admin.ModelAdmin):
