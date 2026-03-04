6ff9910db371ce6ec965909e32051423a3350822
Jesús <jesuslopj0@gmail.com>
2026-03-04 20:34:22 +0100

    Cambiando admin apikeys para el nuevo modelo

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
 from api.models import ApiKey
-# Register your models here.
-@register(ApiKey)
-class APIKeyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'key_hash', 'is_active')
+from django.contrib import messages
+
+@admin.register(ApiKey)
+class ApiKeyAdmin(admin.ModelAdmin):
+    list_display = ("name", "client", "environment", "is_active", "created_at")
+    readonly_fields = ("created_at", "last_used_at", "prefix")
+
+    fields = (
+        "name",
+        "client",
+        "environment",
+        "scopes",
+        "expires_at",
+        "is_active",
+        "prefix",
+        "created_at",
+        "last_used_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        # Solo generar key al crear un nuevo objeto
+        if not change:
+            api_key_obj, raw_key = ApiKey.create_key(
+                client=obj.client,
+                name=obj.name,
+                environment=obj.environment,
+                scopes=obj.scopes,
+                expires_at=obj.expires_at,
+            )
+
+            messages.success(
+                request,
+                f"API Key creada correctamente. Guarda esta clave ahora: {raw_key}"
+            )
+            return
+        else:
+            # Edición normal: solo permite cambiar campos como name, is_active, etc.
+            super().save_model(request, obj, form, change)
